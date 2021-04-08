13b1768eafddad4f02dcc0609575fac73ac7f6a5
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-08 16:30:38 +0600

    add html and javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+var student_details = {
+          name : "Ahsanul Huq <br/>",
+          age : "21 <br/>",
+          study: "BBA runing <br/>",
+          village: "chandrapur <br/>",
+
+}
+document.write("Name =" + student_details.name );
+document.write("Age =" +student_details.age);
+document.write("Study =" +student_details.study);
+document.write("Village =" +student_details.village);
